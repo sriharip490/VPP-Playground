1b386e59b85aed5331e819b8b3c7ddccd73e1163
Srihari Parimi <srihari.linux5@gmail.com>
2026-08-09 00:03:42 +0530

    fixed stop VPP docker compose

--- stop-vpp-nodes.sh
+++ stop-vpp-nodes.sh
@@ -5,8 +5,10 @@ node2="vpp-dev-2"
 
 echo "Stopping VPP nodes ${node1}, ${node2}"
 
-#docker stop vpp-dev-1 vpp-dev-2
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+export USER_NAME=${USER}
 
-docker compose down
+docker compose --env-file dev.env down
 
 
